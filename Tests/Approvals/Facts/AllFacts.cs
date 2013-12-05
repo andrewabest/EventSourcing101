0306b02aa8c29945a863a4ab4a65af70c7398f9b
Andrew Best <andrew.best@readify.net>
2013-12-05 10:11:59 +1000

    Added fact approval tests.

---
+++ Tests/Approvals/Facts/AllFacts.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using Autofac;
+using Core.Domain;
+using FluentAssertions;
+using FluentAssertions.Types;
+using NUnit.Framework;
+
+namespace Tests.Approvals.Facts
+{
+    [TestFixture]
+    public class AllFacts
+    {
+        private readonly IList<string> _blackListedIrregularVerbs = new List<string> { "Become", "Becomming", "Begin", "Come", "Coming", "Do", "Get", "Go", "Have", "Having", "Hide", "Hiding", "Make", "Making", "Pay", "Run", "Take", "Taking" };
+
+        [Test, TestCaseSource(typeof(GetFacts))]
+        public void ShouldBeNamedInPastTense(Type t)
+        {
+            if (t.Name.EndsWith("edFact"))
+                return;
+
+            if (t.Name.EndsWith("MadeFact"))
+                return;
+
+            foreach (var irregularVerb in _blackListedIrregularVerbs)
+            {
+                t.Name.Should().NotContainEquivalentOf(irregularVerb);
+            }
+        }
+
+        [Test, TestCaseSource(typeof(GetFacts))]
+        public void ShouldEndInFact(Type t)
+        {
+            t.Name.Should().EndWith("Fact");
+        }
+
+        [Test, TestCaseSource(typeof(GetFacts))]
+        public void MustBeMarkedSerializable(Type entityType)
+        {
+            entityType.IsSerializable.Should().BeTrue("Facts must be marked serializable.");
+        }
+
+        public class GetFacts : IEnumerable
+        {
+            public IEnumerator GetEnumerator()
+            {
+                var viewModelTypes =
+                    typeof(AggregateRoot).Assembly.GetTypes()
+                        .Where(t => !t.IsInterface && !t.IsAbstract && t.IsAssignableTo<IFact>());
+
+                return viewModelTypes.GetEnumerator();
+            }
+        }
+    }
+}
